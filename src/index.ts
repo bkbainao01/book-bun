ef1f42a9692d61e7069892b77999a079b2efc65f
Sirimanee Pimkaew <sirimanee@imaxxsol.com>
2025-06-13 18:43:49 +0700

    update book-bun-api

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,17 @@ import { bearer } from "@elysiajs/bearer";
 import { authPlugin } from "./plugins/authPlugin";
 import { PrismaClient } from '@prisma/client'
 import { UserSessionService } from './services/userSessionService'; // service จริงของคุณ
+import cors from '@elysiajs/cors'
 
 const prisma = new PrismaClient()
 const sessionService = new UserSessionService();
 const app = new Elysia();
 const appPort: string = process.env.APP_PORT ?? "3000";
 app
+  .use(cors({
+    origin: '*',
+    credentials: true // ถ้าคุณต้องการส่ง cookie, Authorization header
+  }))
   .derive(() => ({db: prisma}))
   .use(
     jwt({
